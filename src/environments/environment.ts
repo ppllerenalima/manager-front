54dd6045be2648afd93207e25bb15f70a0081055
Piero Paolo Llerena Lima <pp.llerenalima@gmail.com>
2025-09-24 20:42:44 -0500

    fix: Corregir la ip en enviorement, para produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 // environment.ts
 export const environment = {
   production: true,
-  apiManager: 'http://localhost:5000/api',          // Tu API normal
-  apiReports: 'http://localhost:5224/api'        // Tu API de Reportes
+  apiManager: 'http://localhost:5000/api', // Tu API normal
+  apiReports: 'http://localhost:5001/api', // Tu API de Reportes
 };
