20f2ab72fc75547f56869106b3c23a20de74814b
Piero Llerena <pp.llerenalima@gmail.com>
2025-10-02 08:11:44 -0500

    fix: Creando los formularios para ConfiguracionGlobal

---
+++ src/app/pages/administration/configuracion-global/configuracion-global.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-configuracion-global',
+  imports: [],
+  template: `<p>configuracion-global works!</p>`,
+  styleUrl: './configuracion-global.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ConfiguracionGlobalComponent { }
