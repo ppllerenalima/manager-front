299dfc324f095b655b1ceadf929fd9acbbc2661a
Piero Llerena <pp.llerenalima@gmail.com>
2025-10-09 07:00:30 -0500

    fix: Validando cambio de contraseña

--- src/app/pages/administration/cuenta/cuenta.component.ts
+++ src/app/pages/administration/cuenta/cuenta.component.ts
@@ -8,13 +8,74 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatTabsModule } from '@angular/material/tabs';
 import { TablerIconsModule } from 'angular-tabler-icons';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatDividerModule } from '@angular/material/divider';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+
+function passwordValidator(control: FormControl): ValidationErrors | null {
+  const value = control.value || '';
+
+  const errors: any = {};
+
+  if (value.length < 8) {
+    errors.minLength = true;
+  }
+  if (!/[A-Z]/.test(value)) {
+    errors.uppercase = true;
+  }
+  if (!/[a-z]/.test(value)) {
+    errors.lowercase = true;
+  }
+  if (!/[0-9]/.test(value)) {
+    errors.number = true;
+  }
+  if (!/[!@#$%^&*()_+\[\]{}:;<>?~\\|]/.test(value)) {
+    errors.specialChar = true;
+  }
+
+  return Object.keys(errors).length ? errors : null;
+}
 
 @Component({
   selector: 'app-cuenta',
-  imports: [MatCardModule, MatIconModule, TablerIconsModule, MatTabsModule, MatFormFieldModule, MatSlideToggleModule, MatSelectModule, MatInputModule, MatButtonModule, MatDividerModule],
+  imports: [
+    MatCardModule,
+    MatIconModule,
+    TablerIconsModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDividerModule,
+
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './cuenta.component.html',
   styleUrl: './cuenta.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppCuentaComponent { }
+export class AppCuentaComponent {
+  hide = true;
+  alignhide = true;
+
+  loginForm = new FormGroup({
+    OldPassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
+    NewPassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+      passwordValidator
+    ]),
+  });
+}
