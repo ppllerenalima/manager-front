52a0c001fcb8952eb15c7620630f6e62dc0d1b30
Piero Llerena <pp.llerenalima@gmail.com>
2025-10-10 13:48:52 -0500

    refactor: Corrigio la ruta enviorement para produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 // environment.ts
 export const environment = {
   production: true,
-  apiManager: 'http://localhost:5000/api', // Tu API normal
-  apiReports: 'http://localhost:5001/api', // Tu API de Reportes
+  apiManager: 'http://192.168.1.38:5000/api', // Tu API normal
+  apiReports: 'http://192.168.1.38:5001/api', // Tu API de Reportes
 };
